cbcbf1d217acc0411e609165237d39ed4f251364
Emanuel Malungo <emalungo@c1r2s5.42luanda.com>
2024-09-27 09:18:50 +0100

    feat: adicionadas funções da libft resolvidas

--- strlcat.c
+++ strlcat.c
@@ -1,29 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*   strlcat.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emalungo <emalungo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/26 13:15:20 by emalungo          #+#    #+#             */
-/*   Updated: 2024/09/27 09:17:09 by emalungo         ###   ########.fr       */
+/*   Created: 2024/09/27 07:55:37 by emalungo          #+#    #+#             */
+/*   Updated: 2024/09/27 09:17:31 by emalungo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-size_t	strlcpy(char *dst, const char *src, size_t size)
+size_t	strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
 
-	i = 0;
-	if (size == 0)
-		return (ft_strlen(src));
-	while (i < (size - 1) && src[i] != '\0')
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (src_len + dst_len);
+	i = dst_len;
+	j = 0;
+	while (i < (size -1) && src[j] != '\0')
 	{
-		dst[i] = src[i];
+		dst[i] = src[j];
 		i++;
+		j++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (src_len + dst_len);
 }
