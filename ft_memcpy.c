53a41480ac6712e364f6ebe82d37ae34d766910c
Emanuel Malungo <emalungo@c1r1s6.42luanda.com>
2024-10-29 08:38:10 +0100

    feat: implementa função ft_memcpy na libft

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,22 @@
 /*   By: emalungo <emalungo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:14:30 by emalungo          #+#    #+#             */
-/*   Updated: 2024/10/29 08:26:01 by emalungo         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:36:17 by emalungo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char			*d;
+	const char		*s;
+
+	d = (char *)dest;
+	s = (const char *)src;
+	if (d == s)
+		return (dest);
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emalungo <emalungo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 12:19:16 by emalungo          #+#    #+#             */
-/*   Updated: 2024/10/29 08:26:35 by emalungo         ###   ########.fr       */
+/*   Updated: 2024/10/29 08:37:16 by emalungo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
